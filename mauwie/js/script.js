9975f09c6a28eecea49f22f5fe8b67c8b27aee51
duyanhdo <anhddhe130724@fpt.edu.vn>
2020-12-24 01:26:32 +0700

    christmas mauwie

---
+++ mauwie/js/script.js
@@ -0,0 +1,237 @@
+jQuery(document).ready(function($) {
+	//wow js
+	new WOW().init();	
+
+	(function () {
+
+		var COUNT = 5000;
+		var masthead = document.querySelector('content');
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
+		var width = masthead.clientWidth;
+		var height = masthead.clientHeight;
+		var i = 0;
+		var active = false;
+
+		function onResize() {
+			width = masthead.clientWidth;
+			height = masthead.clientHeight;
+			canvas.width = width;
+			canvas.height = height;
+			ctx.fillStyle = '#FFF';
+
+			var wasActive = active;
+			active = width > 200;
+
+			if (!wasActive && active)
+				requestAnimFrame(update);
+		}
+
+		var Snowflake = function () {
+			this.x = 0;
+			this.y = 0;
+			this.vy = 0;
+			this.vx = 0;
+			this.r = 0;
+
+			this.reset();
+		}
+
+		Snowflake.prototype.reset = function() {
+			this.x = Math.random() * width;
+			this.y = Math.random() * -height;
+			this.vy = 1 + Math.random() * 3;
+			this.vx = 0.5 - Math.random();
+			this.r = 1 + Math.random() * 2;
+			this.o = 0.5 + Math.random() * 0.5;
+		}
+
+		canvas.style.position = 'absolute';
+		canvas.style.left = canvas.style.top = '0';
+
+		var snowflakes = [], snowflake;
+		for (i = 0; i < COUNT; i++) {
+			snowflake = new Snowflake();
+			snowflake.reset();
+			snowflakes.push(snowflake);
+		}
+
+		function update() {
+
+			ctx.clearRect(0, 0, width, height);
+
+			if (!active)
+				return;
+
+			for (i = 0; i < COUNT; i++) {
+				snowflake = snowflakes[i];
+				snowflake.y += snowflake.vy;
+				snowflake.x += snowflake.vx;
+
+				ctx.globalAlpha = snowflake.o;
+				ctx.beginPath();
+				ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+				ctx.closePath();
+				ctx.fill();
+
+				if (snowflake.y > height) {
+					snowflake.reset();
+				}
+			}
+
+			requestAnimFrame(update);
+		}
+
+  // shim layer with setTimeout fallback
+  window.requestAnimFrame = (function(){
+  	return  window.requestAnimationFrame ||
+  	window.webkitRequestAnimationFrame ||
+  	window.mozRequestAnimationFrame    ||
+  	function( callback ){
+  		window.setTimeout(callback, 500 / 60);
+  	};
+  })();
+
+  onResize();
+  window.addEventListener('resize', onResize, false);
+
+  masthead.appendChild(canvas);
+})();
+
+(function () {
+
+	var COUNT = 200;
+	var masthead = document.querySelector('.footer');
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	var width = masthead.clientWidth;
+	var height = masthead.clientHeight;
+	var i = 0;
+	var active = false;
+
+	function onResize() {
+		width = masthead.clientWidth;
+		height = masthead.clientHeight;
+		canvas.width = width;
+		canvas.height = height;
+		ctx.fillStyle = '#FFF';
+
+		var wasActive = active;
+		active = width > 200;
+
+		if (!wasActive && active)
+			requestAnimFrame(update);
+	}
+
+	var Snowflake = function () {
+		this.x = 0;
+		this.y = 0;
+		this.vy = 0;
+		this.vx = 0;
+		this.r = 0;
+
+		this.reset();
+	}
+
+	Snowflake.prototype.reset = function() {
+		this.x = Math.random() * width;
+		this.y = Math.random() * -height;
+		this.vy = 1 + Math.random() * 3;
+		this.vx = 0.5 - Math.random();
+		this.r = 1 + Math.random() * 2;
+		this.o = 0.5 + Math.random() * 0.5;
+	}
+
+	canvas.style.position = 'absolute';
+	canvas.style.left = canvas.style.top = '0';
+
+	var snowflakes = [], snowflake;
+	for (i = 0; i < COUNT; i++) {
+		snowflake = new Snowflake();
+		snowflake.reset();
+		snowflakes.push(snowflake);
+	}
+
+	function update() {
+
+		ctx.clearRect(0, 0, width, height);
+
+		if (!active)
+			return;
+
+		for (i = 0; i < COUNT; i++) {
+			snowflake = snowflakes[i];
+			snowflake.y += snowflake.vy;
+			snowflake.x += snowflake.vx;
+
+			ctx.globalAlpha = snowflake.o;
+			ctx.beginPath();
+			ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+			ctx.closePath();
+			ctx.fill();
+
+			if (snowflake.y > height) {
+				snowflake.reset();
+			}
+		}
+
+		requestAnimFrame(update);
+	}
+
+  // shim layer with setTimeout fallback
+  window.requestAnimFrame = (function(){
+  	return  window.requestAnimationFrame ||
+  	window.webkitRequestAnimationFrame ||
+  	window.mozRequestAnimationFrame    ||
+  	function( callback ){
+  		window.setTimeout(callback, 1000 / 60);
+  	};
+  })();
+
+  onResize();
+  window.addEventListener('resize', onResize, false);
+
+  masthead.appendChild(canvas);
+})();
+
+const cracker = document.getElementById("crackerWrapper");
+const leftCracker = document.getElementById("leftCracker");
+const rightCracker = document.getElementById("rightCracker");
+const message = document.getElementById("message");
+
+let counter = 0
+
+cracker.addEventListener('click', () => {
+	if(counter < 13){
+		counter++
+	} else{
+		leftCracker.style.animation = "left 1s forwards"
+		rightCracker.style.animation = "right 1s forwards"
+		message.style.animation = "title 1s forwards"
+		cracker.style.transform = "scaleX(1)"
+	}
+})
+
+function Loop(){      
+	window.requestAnimationFrame(Loop);
+	if(counter > 0 && counter < 13){
+		cracker.style.transform = `scaleX(${1 + (counter / 100)})`
+		counter -= 0.05
+	}
+}
+Loop()
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
